4a801372397bb23c4a0cc90de174e52f475ee032
Francesca Chater <francescachater@gmail.com>
2020-08-01 16:19:21 +0100

    Delete and preloader

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import LogItem from './LogItem'
+import Preloader from '../layout/Preloader'
 
 export const Logs = () => {
   const [logs, setLogs] = useState([]); // default is array
@@ -20,7 +21,7 @@ export const Logs = () => {
   }
 
   if (loading) {
-    return <h4>Loading...</h4>;
+    return <Preloader />;
   }
 
   return (
