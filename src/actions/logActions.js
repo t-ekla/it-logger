c3c178fe57a3df721cf511d70f70c135ed45b363
Francesca Chater <francescachater@gmail.com>
2020-08-31 19:49:02 +0100

    logAction, logReducer and types

---
+++ src/actions/logActions.js
@@ -0,0 +1,29 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+export const getLogs = () =>  async dispatch => {
+    try {
+      setLoading();
+
+      const result = await fetch('/logs');
+      const data = await result.json();
+
+      dispatch({
+        type: GET_LOGS,
+        payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: error.response.data
+      })
+    }
+    
+  }
+
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  }
+}
