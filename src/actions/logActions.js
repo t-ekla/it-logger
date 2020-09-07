0ef92bde5a90cfb377ec5bec1cdfd239b832fb57
Francesca Chater <francescachater@gmail.com>
2020-09-07 18:06:25 +0100

    Add action to logActions pulling in type add_logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 
 // Get logs from server
 export const getLogs = () =>  async dispatch => {
@@ -27,4 +27,31 @@ export const setLoading = () => {
   return {
     type: SET_LOADING
   }
+}
+
+// Add new log
+export const addLogs = (log) =>  async dispatch => {
+  try {
+    setLoading();
+
+    const response = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    })
+  }
+  
 }
